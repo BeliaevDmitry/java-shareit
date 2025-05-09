44300ffee7ab20ba5eafa5df1a3ff1f551d9b769
Beliaev Dmitry <79296611647@ya.ru>
2025-05-09 20:45:32 +0300

    User

---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.user.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    Long id;
+
+    @NotBlank
+    String name;
+
+    @NotBlank
+    String email;
+}
