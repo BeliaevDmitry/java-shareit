68a12969b1a60548c17e19bec84b1b2cc9b22535
Beliaev Dmitry <79296611647@ya.ru>
2025-06-01 23:47:21 +0300

    ФЗ15 попытка 1

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,54 +1,59 @@
 package ru.practicum.shareit.item.mapper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.NewItemRequest;
-import ru.practicum.shareit.item.dto.UpdateItemRequest;
+import ru.practicum.shareit.item.dto.NewItemDto;
+import ru.practicum.shareit.item.dto.UpdateItemDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
+import java.util.ArrayList;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ItemMapper {
     public static ItemDto mapToItemDto(Item item) {
         return ItemDto.builder()
                 .id(item.getId())
                 .name(item.getName())
                 .description(item.getDescription())
-                .rentalCount(item.getRentalCount())
                 .available(item.getAvailable())
+                .lastBooking(null)
+                .nextBooking(null)
+                .comments(new ArrayList<>())
                 .build();
     }
 
-    public static ItemDto mapToItem(ItemDto itemDto) {
-        return ItemDto.builder()
+    public static Item mapToItem(ItemDto itemDto, User owner) {
+        return Item.builder()
                 .id(itemDto.getId())
                 .name(itemDto.getName())
                 .description(itemDto.getDescription())
-                .rentalCount(itemDto.getRentalCount())
                 .available(itemDto.getAvailable())
+                .owner(owner)
                 .build();
     }
 
-    public static Item mapToItemNew(NewItemRequest newItemRequest) {
+    public static Item mapToNewItem(NewItemDto requestItemDto) {
         return Item.builder()
-                .name(newItemRequest.getName())
-                .description(newItemRequest.getDescription())
-                .available(newItemRequest.getAvailable())
+                .name(requestItemDto.getName())
+                .description(requestItemDto.getDescription())
+                .available(requestItemDto.getAvailable())
                 .build();
     }
 
-    public static Item mapToItemUpdate(Item item, UpdateItemRequest updateItemRequest) {
-
-        if (updateItemRequest.hasName()) {
-            item.setName(updateItemRequest.getName());
+    public static Item updateItemFields(Item item, UpdateItemDto requestItemDto) {
+        if (requestItemDto.hasName()) {
+            item.setName(requestItemDto.getName());
         }
+        if (requestItemDto.hasDescription()) {
+            item.setDescription(requestItemDto.getDescription());
 
-        if (updateItemRequest.hasDescription()) {
-            item.setDescription(updateItemRequest.getDescription());
         }
-
-        if (updateItemRequest.hasAvailable()) {
-            item.setAvailable(updateItemRequest.getAvailable());
+        if (requestItemDto.hasAvailable()) {
+            item.setAvailable(requestItemDto.getAvailable());
         }
 
         return item;
     }
 }
-
