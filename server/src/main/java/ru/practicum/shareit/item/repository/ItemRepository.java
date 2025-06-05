b947ac38fc550d7e22e7d650aecdbf735c5f3f5d
Beliaev Dmitry <79296611647@ya.ru>
2025-06-06 00:07:12 +0300

    ФЗ16 попытка 2.1

--- server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,10 +1,17 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.shareit.item.model.Comment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByItemId(Long itemId);
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByOwnerIdOrderByIdAsc(Long ownerId);
+
+    @Query("SELECT i FROM Item i WHERE (i.name ILIKE %:text% OR i.description ILIKE %:text%) AND i.available = true")
+    List<Item> findItemsByNameOrDescription(@Param("text") String text);
+
+    List<Item> findByRequestIdOrderByRequestIdDesc(Long requestId);
 }
